530b5ee17f5cdb873eee0b3ee7fa12934bda91e0
Nikodem Kaczmarczyk <nikodemkaczmarczyk48@gmail.com>
2026-07-28 15:29:04 +0200

    new entity and added new dependency for validation

---
+++ api/src/main/java/member/Member.java
@@ -0,0 +1,41 @@
+package member;
+
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Surname cannot be blank")
+    private String surname;
+
+    @NotNull(message = "Age cannot be null")
+    @Min(value = 16, message = "Age must be 16 or higher")
+    private Integer age;
+
+    @NotBlank(message = "Section cannot be blank")
+    private String section;
+
+    private boolean isPassValid;
+
+    @NotNull(message = "Expiry date cannot be null")
+    private LocalDate expiryDate;
+}
