24be9342f08df01a1f1dedcd2cdbdddba3288b94
Nikodem Kaczmarczyk <nikodemkaczmarczyk48@gmail.com>
2026-08-11 18:27:23 +0200

    admin service methods made (#25)
    * admin service methods made

---
+++ api/src/main/java/pl/omnisport/api/admin/AdminService.java
@@ -0,0 +1,52 @@
+package pl.omnisport.api.admin;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+    private final AdminRepository adminRepository;
+
+    public void registerNewAdmin(Admin admin) {
+        admin.setCreatedAt(LocalDate.now());
+        admin.setActive(true);
+        adminRepository.save(admin);
+    }
+
+    public Optional<Admin> findByEmail(String email){
+        return adminRepository.findByEmail(email);
+    }
+
+    @org.springframework.transaction.annotation.Transactional
+    public void deactivateAdmin(Long targetId, Long currentAdminId){
+        if(targetId.equals(currentAdminId))
+            throw new IllegalStateException("You cannot deactivate your account");
+
+        Admin admin = adminRepository.findById(targetId).orElseThrow(
+                () -> new EntityNotFoundException("Admin not found")
+        );
+        admin.setActive(false);
+    }
+
+    @org.springframework.transaction.annotation.Transactional
+    public void recordLogin(Long id){
+        Admin admin = adminRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Admin not found")
+        );
+        admin.setLastLoginAt(LocalDate.now());
+
+    }
+
+    @org.springframework.transaction.annotation.Transactional
+    public void changeAdminRole(Long id, Admin.AdminRole newRole){
+        Admin admin = adminRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Admin not found")
+        );
+        admin.setRole(newRole);
+    }
+}
